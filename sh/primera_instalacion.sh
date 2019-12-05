5146ca102914585095d7d9bb20cd177839f42acf
dani-ddl <dani--ef-@hotmail.com>
2019-12-05 14:02:36 +0100

    modificacion sh

--- sh/primera_instalacion.sh
+++ sh/primera_instalacion.sh
@@ -18,9 +18,9 @@ echo ""
 
 sleep 2
 
-#apt-get update -y
+apt-get update -y
 
-#apt-get upgrade -y
+apt-get upgrade -y
 
 #aumentar el inotify_watch
 
@@ -33,7 +33,7 @@ echo ""
 
 sleep 2
 
-#apt-get install docker.io -y
+apt-get install docker.io -y
 
 echo ""
 echo "Docker instalado correctamente"
@@ -48,7 +48,7 @@ echo ""
 
 sleep 2
 
-#apt-get install docker-compose -y
+apt-get install docker-compose -y
 
 #Añadimos los modulos en extra-addons
 
@@ -123,18 +123,18 @@ echo ""
 echo "Instalando dependencias"
 echo ""
 
-docker ps -a
+#docker ps -a
 
-sleep 10
+#sleep 10
 
-docker ps -a
+#docker ps -a
 
 sleep 1
 
-echo "Instalando xlrd"
-echo ""
+#echo "Instalando xlrd"
+#echo ""
 
-docker exec odoo_odoo_1 pip3 install xlrd
+#docker exec odoo_odoo_1 pip3 install xlrd
 
 echo "¿Quieres levantar Dockerui?(default Sí)"
 echo "Y/y -> Sí"
